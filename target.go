295955dbde6a7d16554187af3d064b54c1f75170
blck-snwmn <whitesnowmancreator@gmail.com>
2021-03-07 16:23:48 +0900

    Add comment

--- target.go
+++ target.go
@@ -1,8 +1,10 @@
 //go:generate go run ./cmd/hello-go-gen -source=./$GOFILE -destination=./out/output.go
 package main
 
+// Foo is samle
 type Foo struct {
-	a int
+	// inner comment
+	a int // a is sample
 }
 
 type Bar struct {
@@ -10,7 +12,9 @@ type Bar struct {
 	b string
 }
 
+// hoge
 type (
+	// Baz1 is sample
 	Baz1 struct {
 		aa int
 	}
